a16637217a283887778e125e6372237d8d31a1cb
davidmrtn07 <davidmrtn07@gmail.com>
2024-03-18 09:12:33 -0500

    Syntax Try

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace SyntaxAndSyntaxSugarExercise
+﻿using System.Runtime.CompilerServices;
+
+namespace SyntaxAndSyntaxSugarExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //int answer = 4; //explicit typing
+            //string response; //explicit typing
+
+            //if (answer < 9)
+            //{
+            //    response = answer + "is less than nine";
+            //}
+            //else
+            //{
+            //    response = answer + "greater than or equal to nine";
+            //}
+
+
+            var answer = 4;
+            var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than or equal to nine";
+
         }
     }
 }
